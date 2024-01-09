8e874bc173965abc171f893088162ece2272e574
abbassimedayoub <6708@holbertonstudents.com>
2024-01-09 16:43:49 +0100

    fix the return

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,11 +6,11 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	unsigned int conteur = 0;
+	unsigned int cont = 0;
 
 	if (!tree)
 		return (0);
 	if ((tree->left || tree->right))
-		conteur = (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
-	return (conteur);
+	cont = (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+	return (cont);
 }
