ce628e8bede04855c995eb1b3cbf3c1f35c97152
abbassimedayoub <6708@holbertonstudents.com>
2024-01-09 16:39:50 +0100

    white space betty error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,22 +7,22 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int left = 0, right = 0;
-    size_t max_height;
+	int left = 0, right = 0;
+	size_t max_height;
 
-    if (!tree)
-        return (0);
-    if (tree->left)
-        left = 1 + binary_tree_height(tree->left);
-    if (tree->right)
-        right = 1 + binary_tree_height(tree->right);
-    if (left > right) 
-    {
-    max_height = left;
-    }
-    else 
-    {
-    max_height = right;
-    }
-return max_height;
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
+	if (left > right)
+	{
+		max_height = left;
+	}
+	else
+	{
+		max_height = right;
+	}
+	return (max_height);
 }
